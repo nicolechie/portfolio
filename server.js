49456d28c3da867e5bcac3227de3a4c58c23d137
Nicole <nicolechie@mac.com>
2016-08-07 17:50:04 +0000

    cleaned up

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-// require('./db/connect');
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// // var itemRoutes = require('./routes/item');
-// var passport = require('passport');
-// var app = express();
-
-// app.use(bodyParser.json());
-// app.use(express.static('client'));
-
-//   app.use(bodyParser.urlencoded({ extended: false }));
-//   app.use(bodyParser.json());
-//   app.use(passport.initialize());
-
-// require('./routes/blog.server.route.js')(app);
-// require('./routes/home.server.route.js')(app);
-// require('./routes/user.server.route.js')(app, passport);
-
-// app.listen(process.env.PORT || 8080, function() {
-//     console.log('Listening on port 8080');
-// });
-
-// exports.app = app;
-
-
 var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -44,15 +19,14 @@ require('./routes/blog.server.route.js')(app);
 require('./routes/home.server.route.js')(app);
 require('./routes/user.server.route.js')(app, passport);
 
-// mongoose.connect('mongodb://nicolechie:rukka2016@ds147995.mlab.com:47995/portfolio').then(function() {
-//     app.listen(process.env.PORT || 8080 );
-// });
+var http = require('http');
+var server = http.Server(app);
 
 mongoose.connect(process.env.MONGODB_NW).then(function(err) {
     if (err) {
         console.log("error", err);
     }
-   app.listen(process.env.PORT || 8080);
+    server.listen(process.env.PORT || 8080);
     console.log("connected", process.env.PORT);
 });
 
